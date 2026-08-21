2c59d06f6fc25ebe3ce318705515ff295ad19b97
JianzhiZhao <34952367+JianzhiZhao@users.noreply.github.com>
2026-08-21 22:15:14 +0800

    Apply custom yellow green color palette

--- src/app/app-icon-design.tsx
+++ src/app/app-icon-design.tsx
@@ -7,7 +7,7 @@ export function AppIconDesign() {
     justifyContent: "center",
     position: "relative",
     overflow: "hidden",
-    background: "linear-gradient(145deg, #2b1810 0%, #120b08 72%)",
+    background: "linear-gradient(145deg, #252b14 0%, #10130b 72%)",
   }}>
     <div style={{
       position: "absolute",
@@ -22,13 +22,13 @@ export function AppIconDesign() {
       paddingLeft: "25%",
       paddingRight: "14%",
       borderRadius: "17%",
-      background: "linear-gradient(150deg, #f2b36f 0%, #c76d37 100%)",
+      background: "linear-gradient(150deg, #fff449 0%, #fed24f 100%)",
       boxShadow: "0 12px 28px rgba(0,0,0,.34)",
     }}>
-      <div style={{ position: "absolute", inset: "0 auto 0 0", width: "17%", borderRadius: "100px 0 0 100px", background: "#4a2617" }} />
-      <div style={{ width: "100%", height: "8%", borderRadius: "100px", background: "#4a2617" }} />
-      <div style={{ width: "76%", height: "8%", borderRadius: "100px", background: "#4a2617" }} />
-      <div style={{ width: "88%", height: "8%", borderRadius: "100px", background: "#4a2617" }} />
+      <div style={{ position: "absolute", inset: "0 auto 0 0", width: "17%", borderRadius: "100px 0 0 100px", background: "#454817" }} />
+      <div style={{ width: "100%", height: "8%", borderRadius: "100px", background: "#454817" }} />
+      <div style={{ width: "76%", height: "8%", borderRadius: "100px", background: "#454817" }} />
+      <div style={{ width: "88%", height: "8%", borderRadius: "100px", background: "#454817" }} />
     </div>
     <div style={{
       position: "absolute",
@@ -40,11 +40,11 @@ export function AppIconDesign() {
       alignItems: "center",
       justifyContent: "center",
       borderRadius: "50%",
-      background: "linear-gradient(145deg, #ffad8f 0%, #ff7657 100%)",
-      border: "6px solid #1c120e",
+      background: "linear-gradient(145deg, #b2d959 0%, #7ec151 100%)",
+      border: "6px solid #181c10",
       boxShadow: "0 8px 20px rgba(0,0,0,.36)",
     }}>
-      <div style={{ width: "54%", height: "54%", borderRadius: "50%", border: "4px solid rgba(74,38,23,.62)" }} />
+      <div style={{ width: "54%", height: "54%", borderRadius: "50%", border: "4px solid rgba(50,68,24,.62)" }} />
     </div>
   </div>;
 }
